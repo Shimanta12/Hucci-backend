617079a567d4cc9d5c6fc246145851a060b525f7
Shimanta12 <sbemon1356@gmail.com>
2024-05-31 22:28:24 +0600

    credential hidden

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config({ path: ".env.local" });
 
 const app = express();
 const port = 3000;
@@ -12,8 +13,7 @@ app.get("/", (req, res) => {
   res.send("Hello from server!");
 });
 
-const uri =
-  "mongodb+srv://Shimanto:md1bHIvFknwsrkUh@cluster0.fhjxu7c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhjxu7c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
